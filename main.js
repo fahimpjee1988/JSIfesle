0a39558fda745cce7b6e13de7df6ea0b513f2076
fahimpjee1988 <adjmal.ra@gmail.com>
2020-07-10 11:09:03 +0200

    ifelse

---
+++ main.js
@@ -0,0 +1,18 @@
+
+//Lev1_1_js-vertiefung_conditional-statements_score
+
+
+function myFunction(){
+    console.log("test")
+    if (document.getElementById("age").value >=18){
+        console.log(true);
+        document.getElementById("id").innerHTML = "Volljährig!"
+    }else{
+        document.getElementById("id").innerHTML = "Minderjährig!"
+
+    }
+}
+
+//
+
+
