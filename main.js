4931ee39417c2157eb8b2f8d7e38f98218ed2c6e
fahimpjee1988 <adjmal.ra@gmail.com>
2020-07-10 11:58:30 +0200

    fertig

--- main.js
+++ main.js
@@ -13,6 +13,33 @@ function myFunction(){
     }
 }
 
-//
+//Lev1_2_js-vertiefung_conditional-statements-quelitaet
 
+function less(){
+    if (document.getElementById("null").checked){
+        document.getElementById("h2").innerHTML = "Slecht"
+    } else if (document.getElementById("drei").checked){
+
+            document.getElementById("h2").innerHTML = "'okay'"
+        }else if (document.getElementById("sechs").checked){
+            
+            document.getElementById("h2").innerHTML = "gut"
+
+        }else if(document.getElementById("acht").checked){
+
+            document.getElementById("h2").innerHTML = "super'"
+
+        }
+    }
+
+//Lev1_3_js-vertiefung_conditional-statements_mit-form
+
+function greaterThen(){
+    if(document.getElementById("input").value>=18){
+        document.getElementById("shisha").innerHTML = "'Ja. Du kannst Shisha rauchen"
+
+    }else if(document.getElementById("input").value<18){
+        document.getElementById("shisha").innerHTML = "Du darfst leider noch nicht Shisha rauchen"
+    }
+}
 
